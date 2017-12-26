9eef63bc01acd85bd5ed232d45f250fd2ddd6859
ChenYong <jiyinyiyong@dingtalk.com>
2017-12-26 14:37:53 +0800

    skeleton of formatter

--- src/favored_edn/core.cljs
+++ src/favored_edn/core.cljs
@@ -1,4 +1,72 @@
 
-(ns favored-edn.core )
+(ns favored-edn.core (:require [clojure.string :as string]))
 
-(defn write-edn ([data] (write-edn data 0)) ([data indent] "EDN"))
+(declare write-edn)
+
+(declare write-set)
+
+(declare write-vector)
+
+(declare write-map)
+
+(defn break-line [x] (string/join (str "\n" (string/join "" (repeat x " ")))))
+
+(defn literal? [x]
+  (or (number? x) (string? x) (keyword? x) (symbol? x) (nil? x) (boolean? x)))
+
+(defn inc2 [x] (+ x 1))
+
+(defn write-map [dict indent]
+  (if (or (empty? dict) (every? literal? (vals dict)))
+    (pr-str dict)
+    (if (every? string? (keys dict))
+      (let [next-indent (inc2 indent)]
+        (str
+         "{"
+         (break-line next-indent)
+         (->> dict
+              (sort-by first)
+              (map (fn [[k v]] (str (pr-str k) " " (write-edn v (inc2 next-indent)))))
+              (string/join (break-line next-indent)))
+         (break-line indent)
+         "}"))
+      (let [simple-fields (->> dict (filter (fn [[k v]] (literal? v))))
+            complicated-fields (->> dict (filter (fn [[k v]] (not (literal? v)))))
+            next-indent (inc2 indent)]
+        (str
+         "{"
+         (->> simple-fields
+              (map (fn [[k v]] (str (pr-str k) " " (pr-str v))))
+              (string/join " "))
+         (break-line (inc2 next-indent))
+         (->> complicated-fields
+              (map (fn [[k v]] (str (pr-str k) " " (write-edn v (inc2 next-indent)))))
+              (string/join (break-line (inc2 next-indent))))
+         (break-line indent)
+         "}")))))
+
+(defn write-vector [xs indent]
+  (str
+   "["
+   (let [new-indent (inc2 indent)]
+     (->> xs (map (fn [x] (write-edn x new-indent))) (string/join (break-line new-indent))))
+   "]"))
+
+(defn write-set [xs indent]
+  (str
+   "#{"
+   (let [new-indent (inc2 indent)]
+     (->> xs (map (fn [x] (write-edn x new-indent))) (string/join (break-line new-indent))))
+   "}"))
+
+(defn write-edn
+  ([data] (write-edn data 0))
+  ([data indent]
+   (cond
+     (map? data) (write-map data indent)
+     (vector? data) (write-vector data indent)
+     (set? data) (write-set data indent)
+     (number? data) (pr-str data)
+     (string? data) (pr-str data)
+     (boolean? data) (pr-str data)
+     :else (pr-str data))))
